d6d22db6d22d730c776f97d04fc3d897a8c37e0b
CheraHamza <cherahamza1@gmail.com>
2025-02-01 20:59:02 +0100

    write capitalize function based off the test

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function capitalize(string) {
-	return "A";
+	const words = string.split(" ");
+	words.forEach((word, index) => {
+		const characters = word.split("");
+		if (characters[0]) {
+			characters[0] = characters[0].toUpperCase();
+		}
+
+		words[index] = characters.join("");
+	});
+
+	return words.join(" ");
 }
 
 module.exports = capitalize;
