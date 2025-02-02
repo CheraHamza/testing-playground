a9ffee6db60b3c4b418a1056cb63280aa5a98842
CheraHamza <cherahamza1@gmail.com>
2025-02-02 11:54:39 +0100

    write caesarCipher function based on tests

--- script.js
+++ script.js
@@ -31,4 +31,37 @@ const calculator = {
 	},
 };
 
-module.exports = { capitalize, reverseString, calculator };
+function findShiftedIndex(currentIndex, shiftFactor) {
+	for (let i = 0; i < shiftFactor; i++) {
+		if (currentIndex < 25) {
+			currentIndex++;
+		} else {
+			currentIndex = 0;
+		}
+	}
+
+	return currentIndex;
+}
+
+function caesarCipher(string, shiftBy) {
+	const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+	const characters = string.split("");
+
+	characters.forEach((char, index) => {
+		const isUpperCase = char == char.toUpperCase() ? true : false;
+		const orderInAlph = alphabet.indexOf(char.toLowerCase());
+
+		if (orderInAlph != -1) {
+			const newCharIndex = findShiftedIndex(orderInAlph, shiftBy);
+			characters[index] = isUpperCase
+				? alphabet[newCharIndex].toUpperCase()
+				: alphabet[newCharIndex];
+		}
+	});
+
+	return characters.join("");
+}
+
+
+
+module.exports = { capitalize, reverseString, calculator, caesarCipher };
